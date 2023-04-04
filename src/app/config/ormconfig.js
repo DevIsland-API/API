6fa1b8b7ee1c33b41a31f5d6be388d9b3fe45266
Joao Medeiros <joao.gallina@einstein.br>
2023-04-04 12:18:47 -0300

    chore: ormconfig file create

---
+++ src/app/config/ormconfig.js
@@ -0,0 +1,21 @@
+import "../";
+const config = {
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities:
+    process.env.NODE_ENV === "local"
+      ? ["../entities/*.ts"]
+      : ["../../dist/src/app/entities/*.js"],
+  cli: {
+    migrationsDir: "../database/migrations",
+  },
+  synchronize: false,
+  logging: true,
+  timezone: "America/Sao_Paulo",
+};
+
+module.exports = config;
