a53359b138222305577388f18831da2520ee1d00
Joao Medeiros <joao.gallina@einstein.br>
2023-04-11 09:37:04 -0300

    feat: ensure GetFormController uses correct interface

--- src/app/controllers/management/GetFormController.ts
+++ src/app/controllers/management/GetFormController.ts
@@ -1,12 +1,16 @@
-import { ListFormService } from "@/app/services/ListFormService";
+import { GetFormService } from "@/app/services/GetFormService";
 import { Request, Response } from "express";
+import { IGetFormDTO } from "./dto/GetFormDTO";
 
 export class GetFormController {
-  static async handle(request: Request, response: Response): Promise<Response> {
+  static async handle(
+    request: Request<IGetFormDTO>,
+    response: Response
+  ): Promise<Response> {
     try {
-      const listForm = await ListFormService.execute();
+      const getForm = await GetFormService.execute(request.params.id);
 
-      return response.status(200).send(listForm);
+      return response.status(200).send(getForm);
     } catch (error) {
       console.log("error -->", error);
       return response.status(500).send({ error: "Internal Server Error" });
