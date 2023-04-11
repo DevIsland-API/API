faaf1895c2198ceb62ff806a34f0b4286157dfdc
Joao Medeiros <joao.gallina@einstein.br>
2023-04-11 09:37:31 -0300

    refactor: refactor on params of DeleteFormService

--- src/app/services/DeleteFormService.ts
+++ src/app/services/DeleteFormService.ts
@@ -2,7 +2,7 @@ import { Chamado } from "../entities/Chamado";
 import { FormRepository } from "../repositories/FormRepository";
 
 export class DeleteFormService {
-  static async execute(form: any, formId: number): Promise<Chamado> {
+  static async execute(formId: number): Promise<Chamado> {
     return await FormRepository.deactivate(formId);
   }
 }
