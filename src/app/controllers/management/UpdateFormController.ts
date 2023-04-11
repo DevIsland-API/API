f5e3dafbd7d728494a0f96273f5cbce2bac712aa
Joao Medeiros <joao.gallina@einstein.br>
2023-04-11 08:37:32 -0300

    chore: UpdateFormController update

--- src/app/controllers/management/UpdateFormController.ts
+++ src/app/controllers/management/UpdateFormController.ts
@@ -9,9 +9,10 @@ export class UpdateFormController {
 
       const createdForm = await CreateFormService.execute(form);
 
-      return response
-        .status(200)
-        .send({ message: "Chamado criado com sucesso", created: createdForm });
+      return response.status(200).send({
+        message: "Chamado atualizado com sucesso!",
+        created: createdForm,
+      });
     } catch (error) {
       console.log("error -->", error);
       return response.status(500).send({ error: "Internal Server Error" });
