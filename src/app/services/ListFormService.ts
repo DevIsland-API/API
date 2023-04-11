51dee2824e2f050cbad2116464db020866606350
Joao Medeiros <joao.gallina@einstein.br>
2023-04-11 09:26:46 -0300

    feat: ListFormService

---
+++ src/app/services/ListFormService.ts
@@ -0,0 +1,11 @@
+import { Chamado } from "../entities/Chamado";
+import { FormRepository } from "../repositories/FormRepository";
+
+export class ListFormService {
+  static async execute(): Promise<Chamado[]> {
+    const forms = await FormRepository.getAll();
+    // assembler para tratamento do body
+
+    return forms; // tratar no assembler, salvar numa variável e retornar
+  }
+}
