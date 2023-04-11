07394c87acd4a80352662b5043144ac038e36e7d
Joao Medeiros <joao.gallina@einstein.br>
2023-04-11 09:30:34 -0300

    feat: implementing formRoutes in application

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { formRoutes } from "./formRoutes";
 import { miscelaneousRoutes } from "./miscelaneousRoutes";
 
 export const appRoutes = (): Router => {
   const routes = Router();
   routes.use("/healthCheck", miscelaneousRoutes());
+  routes.use("/formManagement", formRoutes());
 
   return routes;
 };
