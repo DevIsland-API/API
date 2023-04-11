2884e2c1975f4007bc6f09656b680c4a544644cb
Joao Medeiros <joao.gallina@einstein.br>
2023-04-11 09:50:11 -0300

    refactor: project management refactor. folder organization

--- src/app/services/DeleteFormService.ts
+++
@@ -1,8 +0,0 @@
-import { Chamado } from "../entities/Chamado";
-import { FormRepository } from "../repositories/FormRepository";
-
-export class DeleteFormService {
-  static async execute(formId: number): Promise<Chamado> {
-    return await FormRepository.deactivate(formId);
-  }
-}
