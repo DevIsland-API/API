d97169a9bc0f6371be939818e806f78aca15b892
Joao Medeiros <joao.gallina@einstein.br>
2023-04-11 09:29:21 -0300

    feat: add all management form routes

--- src/app/routes/formRoutes.ts
+++ src/app/routes/formRoutes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import "../config/dotenv";
 import { CreateFormController } from "../controllers/management/CreateFormController";
+import { DeleteFormController } from "../controllers/management/DeleteFormController";
+import { GetFormController } from "../controllers/management/GetFormController";
+import { ListFormController } from "../controllers/management/ListFormController";
 import { UpdateFormController } from "../controllers/management/UpdateFormController";
 
 export const formRoutes = (): Router => {
@@ -8,6 +11,9 @@ export const formRoutes = (): Router => {
 
   route.post("/", CreateFormController.handle);
   route.patch("/:id", UpdateFormController.handle);
+  route.delete(":id", DeleteFormController.handle);
+  route.get("/:id", GetFormController.handle);
+  route.get("/", ListFormController.handle);
 
   return route;
 };
