49c5831606404defc7b908150549ee8b969c74ec
Joao Medeiros <joao.gallina@einstein.br>
2023-04-11 09:51:05 -0300

    refactor: project management refactor. folder organization

---
+++ src/app/services/management/CreateFormService.ts
@@ -0,0 +1,12 @@
+import { Chamado } from "../../entities/Chamado";
+import { FormRepository } from "../../repositories/FormRepository";
+
+export class CreateFormService {
+  static async execute(form: any): Promise<Chamado> {
+    // validação
+    // assembler para tratamento do body
+    // const assembledForm = createFormAssembler().build()
+
+    return await FormRepository.createForm(form);
+  }
+}
