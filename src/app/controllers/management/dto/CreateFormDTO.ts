b0029e264c2281ec611c571cc0c387ddfe6ad794
Joao Medeiros <joao.gallina@einstein.br>
2023-04-17 09:17:05 -0300

    feat: typo of entry body

--- src/app/controllers/management/dto/CreateFormDTO.ts
+++ src/app/controllers/management/dto/CreateFormDTO.ts
@@ -1,1 +1,8 @@
-export interface ICreateFormDTO {}
+export interface ICreateFormDTO {
+  titulo: string;
+  descricao: string;
+  tipo: number;
+  status: number;
+  usuarioRequisitador: number;
+  arquivo: string;
+}
