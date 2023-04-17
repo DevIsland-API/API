278072163f9c73f26f3779f074c4e61027e3d3ad
Joao Medeiros <joao.gallina@einstein.br>
2023-04-17 09:36:08 -0300

    feat: typo of entry body and refactor on imports

--- src/app/services/management/CreateFormService.ts
+++ src/app/services/management/CreateFormService.ts
@@ -1,17 +1,17 @@
-import { encode } from "node-base64-image";
+import { ICreateFormDTO } from "../../controllers/management/dto/CreateFormDTO";
 import { Chamado } from "../../entities/Chamado";
+import { FileRepository } from "../../repositories/FileRepository";
 import { FormRepository } from "../../repositories/FormRepository";
 
 export class CreateFormService {
-  static async execute(form: any): Promise<Chamado> {
-    // validação
-    // assembler para tratamento do body
-    // const assembledForm = createFormAssembler().build()
+  static async execute(receivedForm: ICreateFormDTO): Promise<Chamado> {
+    // let formToCreate;
+    // formToCreate = formToCreate.omit(receivedForm.arquivo);
+    const form = await FormRepository.createForm(receivedForm);
+    const file = await FileRepository.create(form.id, receivedForm.arquivo);
 
-    const encodedImage = await encode(form.image);
+    const createdForm = Object.assign({}, form, { arquivo: file.id });
 
-    return await FormRepository.createForm(
-      Object.assign({}, form, { imagem: encodedImage })
-    );
+    return createdForm;
   }
 }
