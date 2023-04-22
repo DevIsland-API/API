386915d3124d930552679ade15e51993a676bb0c
Joao Medeiros <joao.gallina@einstein.br>
2023-04-22 13:30:38 -0300

    feat: add getByFormId method in ArquivoMidia repository and refactor on update method

--- src/app/repositories/FileRepository.ts
+++ src/app/repositories/FileRepository.ts
@@ -16,12 +16,37 @@ export class FileRepository {
     }
   }
 
+  static async getOneByForm(formId: number): Promise<ArquivoMidia> {
+    try {
+      const foundFile = await ArquivoMidia.findOne({
+        where: {
+          idChamado: formId,
+        },
+      });
+
+      return foundFile;
+    } catch (error) {
+      console.log(error);
+      throw new Error();
+    }
+  }
+
   static async create(formId: number, fileName: string): Promise<ArquivoMidia> {
     try {
       return await ArquivoMidia.create({
         arquivo: fileName,
         idChamado: formId,
       }).save();
+    } catch (error) {
+      console.log("error -->", error);
+      throw new Error();
+    }
+  }
+
+  static async update(formId: number, fileName: string): Promise<ArquivoMidia> {
+    try {
+      await ArquivoMidia.update({ idChamado: formId }, { arquivo: fileName });
+      return await this.getOneByForm(formId);
     } catch (error) {
       console.log("error -->", error);
       throw new Error();
